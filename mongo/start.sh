0c3501d43e2e9ac0c866f573eae41174cb937b74
lookTT <zltdhr@gmail.com>
2019-09-24 13:44:43 +0800

    Create start.sh

---
+++ mongo/start.sh
@@ -0,0 +1,13 @@
+# 创建一个挂载盘
+docker volume create mongodb
+# 拉镜像
+docker pull mongo
+# 启动docker
+docker run --name mongodb -p 27017:27017 -v mongodb:/data/db/ -d mongo --auth
+
+# docker exec -it mongodb /bin/bash
+# mongo
+#  > use admin
+#  > db.createUser({user:'root',pwd:'root',roles:[{role:"root",db:"admin"}]})
+#  > use test
+#  > db.createUser({user:'test',pwd:'test',roles:[{role:"readWrite",db:"test"}]})
