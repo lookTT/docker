7758f1024466d952aa8f0ae70b8e651ba2f23356
lookTT <zltdhr@gmail.com>
2020-05-21 13:04:46 +0800

    Update start.sh

--- v2ray/start.sh
+++ v2ray/start.sh
@@ -1,1 +1,1 @@
-docker run -d --name v2ray -v /home/v2ray:/etc/v2ray -p 12345:12345 --restart=always v2ray/official  v2ray -config=/etc/v2ray/config.json
+docker run -d --name v2ray -v /home/v2ray:/etc/v2ray -p 40725:40725 --restart=always v2ray/official  v2ray -config=/home/v2ray/config.json
