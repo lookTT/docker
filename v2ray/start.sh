13a5b57f15dfc77665c71077f74ff0117ddcf17f
lookTT <zltdhr@gmail.com>
2022-10-21 15:55:07 +0800

    Update start.sh

--- v2ray/start.sh
+++ v2ray/start.sh
@@ -2,4 +2,4 @@ yum install docker
 systemctl enable docker
 systemctl start docker
 
-docker run -d --name v2ray -v /home/v2ray:/etc/v2ray -p 12345:12345 --restart=always v2ray/official v2ray -config=/home/v2ray/config.json
+docker run -d --name v2ray -v /home/v2ray:/etc/v2ray/ -p 12345:12345 --restart=always v2fly/v2fly-core v2ray
